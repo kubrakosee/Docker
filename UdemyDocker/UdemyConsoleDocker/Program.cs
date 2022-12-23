236f913fabba67c319600f7e45bd342d89b2ecb7
Kübra Köse <kbr__1544@hotmail.com>
2022-12-23 17:56:56 +0300

    9).Net Core Console uygulamasını dockerize yapmak-1

--- UdemyDocker/UdemyConsoleDocker/Program.cs
+++ UdemyDocker/UdemyConsoleDocker/Program.cs
@@ -4,9 +4,21 @@ namespace UdemyConsoleDocker
 {
     class Program
     {
+        /// <summary>
+        /// şimdi kodu yazdık build edicez.Build ettikten sonra tekrar klasörümü gel
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int i = 1;
+
+            while (i < 1000)
+            {
+
+                Console.WriteLine("Hello World!" + i);
+                i++;
+                System.Threading.Thread.Sleep(1000);
+            }
         }
     }
 }
